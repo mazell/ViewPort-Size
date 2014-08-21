8f55b7573f63c6bba9c8c146b6b30b18430d9992
mazell <mattias@zellout.se>
2014-08-21 09:49:39 +0200

    Create vpSize.js
    First commit

---
+++ vpSize.js
@@ -0,0 +1,100 @@
+
+
+
+
+(function(funcName, baseObj) {
+    // The public function name defaults to window.docReady
+    // but you can pass in your own object and own function name and those will be used
+    // if you want to put them in a different namespace
+    funcName = funcName || "docReady";
+    baseObj = baseObj || window;
+    var readyList = [];
+    var readyFired = false;
+    var readyEventHandlersInstalled = false;
+    
+    // call this when the document is ready
+    // this function protects itself against being called more than once
+    function ready() {
+        if (!readyFired) {
+            // this must be set to true before we start calling callbacks
+            readyFired = true;
+            for (var i = 0; i < readyList.length; i++) {
+                // if a callback here happens to add new ready handlers,
+                // the docReady() function will see that it already fired
+                // and will schedule the callback to run right after
+                // this event loop finishes so all handlers will still execute
+                // in order and no new ones will be added to the readyList
+                // while we are processing the list
+                readyList[i].fn.call(window, readyList[i].ctx);
+            }
+            // allow any closures held by these functions to free
+            readyList = [];
+        }
+    }
+    
+    function readyStateChange() {
+        if ( document.readyState === "complete" ) {
+            ready();
+        }
+    }
+    
+    // This is the one public interface
+    // docReady(fn, context);
+    // the context argument is optional - if present, it will be passed
+    // as an argument to the callback
+    baseObj[funcName] = function(callback, context) {
+        // if ready has already fired, then just schedule the callback
+        // to fire asynchronously, but right away
+        if (readyFired) {
+            setTimeout(function() {callback(context);}, 1);
+            return;
+        } else {
+            // add the function and context to the list
+            readyList.push({fn: callback, ctx: context});
+        }
+        // if document already ready to go, schedule the ready function to run
+        if (document.readyState === "complete") {
+            setTimeout(ready, 1);
+        } else if (!readyEventHandlersInstalled) {
+            // otherwise if we don't have event handlers installed, install them
+            if (document.addEventListener) {
+                // first choice is DOMContentLoaded event
+                document.addEventListener("DOMContentLoaded", ready, false);
+                // backup is window load event
+                window.addEventListener("load", ready, false);
+            } else {
+                // must be IE
+                document.attachEvent("onreadystatechange", readyStateChange);
+                window.attachEvent("onload", ready);
+            }
+            readyEventHandlersInstalled = true;
+        }
+    }
+})("docReady", window);
+
+docReady(function() {
+   
+    function init(){
+        showViewportSize();
+        window.addEventListener('resize', function() {
+            showViewportSize();
+        });
+    };
+    function showViewportSize(){
+       var width = document.width || window.outerWidth;
+        var height = document.height || window.outerHeight;
+       //var cDiv = document.getElementById( "sb-rulers" ); 
+       var container = document.getElementById( "pxcontainer") || document.createElement( "div" );
+       container.id = 'pxcontainer';
+       container.style['background-color'] = "#f3f3f3";
+       container.style.position = "absolute";
+       container.style.top = "0px";
+       container.style.right = "0px";
+       document.body.appendChild( container );                  
+       container.innerHTML = width + 'px x ' + height + 'px';
+       //$('#height').text(the_height);
+    };
+
+    init();
+        
+});
